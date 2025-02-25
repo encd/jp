db3808bf193b0417b7ee7645327ad2fff5c70950
ende <165443758+encd@users.noreply.github.com>
2025-02-26 02:16:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(sessionMiddleware);
 // Socket.IO setup
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://jp-mzlm.onrender.com/",
     methods: ["GET", "POST"],
     credentials: true
   }
